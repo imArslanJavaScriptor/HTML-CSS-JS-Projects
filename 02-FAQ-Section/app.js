194adb3299a2746d56742422b1089eedb01aa9ca
Muhammad Arslan <imarslan7061@gmail.com>
2025-01-09 08:00:45 +0500

    Faq Section Feature

--- 02-FAQ-Section/app.js
+++ 02-FAQ-Section/app.js
@@ -1,30 +1,13 @@
-// // Select all FAQ questions
-// const faqItems = document.querySelectorAll(".faq-item");
+document.addEventListener("DOMContentLoaded", () => {
+  const faqItems = document.querySelectorAll(".faq-item")
+  faqItems.forEach((item) => {
+    const questionEl = item.querySelector(".faq-question")
+    const ansEl = item.querySelector(".faq-answer")
+    const tolggleEl = item.querySelector(".faq-toggle")
 
-// faqItems.forEach((item) => {
-//   const question = item.querySelector(".faq-question");
-//   const answer = item.querySelector(".faq-answer");
-//   const toggle = item.querySelector(".faq-toggle");
-
-//   // Add click event listener
-//   question.addEventListener("click", () => {
-//     // Toggle the active class
-//     answer.classList.toggle("active");
-
-//     // Change the toggle sign
-//     toggle.textContent = answer.classList.contains("active") ? "-" : "+";
-//   });
-// });
-
-
-const faqItems = document.querySelectorAll(".faq-item")
-faqItems.forEach((item) => {
-    const question = item.querySelector(".faq-question");
-      const answer = item.querySelector(".faq-answer");
-      const toggle = item.querySelector(".faq-toggle");
-
-    question.addEventListener("click", () => {
-        answer.classList.toggle("active")
-        toggle.textContent = answer.classList.contains("active") ? "-" : "+"
+    questionEl.addEventListener("click", () => {
+      ansEl.classList.toggle("active")
+      tolggleEl.textContent  = ansEl.classList.contains("active") ? "-" : "+"  
     })
-});
+  })
+})
